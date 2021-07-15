b7d4de5539ead9b1d523e4d0510c8dad84b4e1dd
kamina04 <adrianna.kaminska@bbc.co.uk>
2021-07-16 00:13:45 +0100

    Works nively in archives

--- popup.js
+++ popup.js
@@ -2,11 +2,11 @@ let simplifiedEng = document.getElementById("Words");
 let idioms = document.getElementById("Idioms");
 let transcript = document.getElementById("Transcript");
 
-simplifiedEng.addEventListener("change", async () => {
+transcript.addEventListener("change", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: runSimplifiedEng,
+        function: runTranscript,
     });
 });
 
@@ -18,22 +18,46 @@ idioms.addEventListener("change", async () => {
     });
 });
 
-transcript.addEventListener("change", async () => {
+simplifiedEng.addEventListener("change", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: runTranscript,
+        function: runSimplifiedEng,
     });
 });
 
 function popUp() {
-    const dialogBox = document.getElementById("dialogBox");
-    return dialogBox.style.visibility = 
-    (dialogBox.style.visibility != "visible") ? "visible" : "hidden";
+    // const dialogBox = document.getElementById("dialogBox");
+    // return dialogBox.style.visibility = 
+    // (dialogBox.style.visibility != "visible") ? "visible" : "hidden";
+    console.log('works');
+}
+
+function runTranscript () {
+    // const div = document.createElement("div");
+    // document.body.appendChild(div);
+    const videoIPlayer = document.getElementsByClassName("hero-player"); //works for iPlayer when video is on
+
+    const archives = document.getElementsByClassName("gel-layout__item timeline__buttons");
+
+    const div = document.createElement("div");
+    div.classList.add('container_button');
+    const bbc_button = document.createElement('button');
+    bbc_button.type = 'button';
+    bbc_button.innerHTML = 'Press me';    
+    bbc_button.onclick = function(req, response) { 
+
+        fetch('http://localhost:5000')
+        .then(response => response.xml())
+        .catch(error => console.error(error));
+     };
+    
+    
+    // videoIPlayer[0].append(bbc_button);
+    archives[0].append(bbc_button);
 }
 
 runSimplifiedEng = async () => {
-    document.querySelector(".myDiv").addEventListener("click", e => console.log(event.target));
 }
 
 function fetchData() {
